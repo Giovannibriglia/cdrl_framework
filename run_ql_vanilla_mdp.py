6a9f35dcb6ee7f68ad616557613470ca48aae233
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2024-06-27 14:37:03 +0200

    update algos

---
+++ run_ql_vanilla_mdp.py
@@ -0,0 +1,8 @@
+from navigation.trainer import run_simulations
+from path_repo import GLOBAL_PATH_REPO
+import pyglet
+
+if __name__ == '__main__':
+    pyglet.options['headless'] = True
+    path_file = f'{GLOBAL_PATH_REPO}/config_simulations/qlearning_vanilla_mdp.yaml'
+    run_simulations(path_file, False)
